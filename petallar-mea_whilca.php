a75a744111866709c0ce536b2213a4872c668b30
Sir Clark <reneilleclark.nam-ay@bisu.edu.ph>
2026-04-30 10:04:04 +0800

    DOne

--- petallar-mea_whilca.php
+++ petallar-mea_whilca.php
@@ -1,17 +1,1 @@
-<?php
-// Basic PHP script
-
-$name = "World";
-
-// Output text
-echo "Hello, " . $name . "!";
-
-// Conditional example
-$age = 18;
-
-if ($age >= 18) {
-    echo "\nYou are an adult.";
-} else {
-    echo "\nYou are a minor.";
-}
-?>
+Done
