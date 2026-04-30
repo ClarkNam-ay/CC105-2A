a79ea4dbf561d9db42c37abe8348727a73d77880
meawhilca <petallarmeawhilca@gmail.com>
2026-04-30 10:02:48 +0800

    push

--- petallar-mea_whilca.php
+++ petallar-mea_whilca.php
@@ -1,1 +1,17 @@
-jdgfasdfijasdgfkjasdgfjfhadjkh
+<?php
+// Basic PHP script
+
+$name = "World";
+
+// Output text
+echo "Hello, " . $name . "!";
+
+// Conditional example
+$age = 18;
+
+if ($age >= 18) {
+    echo "\nYou are an adult.";
+} else {
+    echo "\nYou are a minor.";
+}
+?>
